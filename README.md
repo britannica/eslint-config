25fd89436697711c33b655fd1ff60aa79d4fbd7f
Ryan Walters <rwalters@eb.com>
2018-05-31 09:44:50 -0500

    basic setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# @britannica/eslint-config
+
+Extends [Airbnb's style guide](https://github.com/airbnb/javascript), specifically the [eslint-config-airbnb](https://www.npmjs.com/package/eslint-config-airbnb) package.
+
+#### Requirements
+
+```
+npm@5.2.0+
+```
+
+#### Installation
+
+```
+npx install-peerdeps --dev @britannica/eslint-config
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: 'airbnb',
+  "rules": {
+    "brace-style": ["error", "stroustrup"],
+    "linebreak-style": 0,
+    "lines-around-comment": ["error", {
+      "allowBlockStart": true,
+      "beforeBlockComment": true,
+      "afterBlockComment": true,
+      "beforeLineComment": true,
+      "afterLineComment": true
+    }],
+    "max-len": 0,
+    "newline-before-return": "error",
+    "newline-after-var": "error",
+    "no-shadow": ["error", {
+      "allow": ["i"]
+    }],
+    "no-use-before-define": 0,
+    "object-curly-newline": ["error", {
+      "consistent": true
+    }],
+    "padding-line-between-statements": ["error",
+      { "blankLine": "always", "prev": "*", "next": "block-like" },
+      { "blankLine": "always", "prev": "block-like", "next": "*" },
+      { "blankLine": "always", "prev": "*", "next": ["const", "let"] },
+      { "blankLine": "any", "prev": ["const", "let"], "next": ["const", "let"] }
+    ],
+    "radix": 0,
+    "space-before-blocks": "error",
+    "react/jsx-filename-extension": 0
+  },
+};
