64fe99f6f6612fb96825f1ee480b2961d24fb637
Ryan Walters <rwalters@eb.com>
2019-08-12 16:21:39 -0500

    disable react/jsx-props-no-spreading rule

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ module.exports = {
     "react/jsx-filename-extension": 0,
     "react/jsx-one-expression-per-line": 0,
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": 0,
   },
 };
